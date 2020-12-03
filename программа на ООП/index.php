4e306b69d325545251f86cda4c15e169bd9b27cd
Gogs <gogs@fake.local>
2020-12-03 17:33:38 +0500

    second

--- программа на ООП/index.php
+++ программа на ООП/index.php
@@ -11,80 +11,115 @@
 		<div class="sun">
 			<img src="images/sun.png">
 		</div>
+
+		<div class="orbit_mercury"></div>
 		<div class="Mercury">
 			<img src="images/mercury.png">
 		</div>
+
+		<div class="orbit_venus"></div>
 		<div class="Venus">
 			<img src="images/venera.png">
 		</div>
+
+		<div class="orbit_erth"></div>
 		<div class="Erth">
 			<img src="images/zemla.png">
 			<div class="Moon">
+				<div class="orbit_moon"></div>
 				<img src="images/moon.png">
 			</div>
 		</div>
+
+		<div class="orbit_mars"></div>
 		<div class="Mars">
 			<img src="images/mars.png">
 			<div class="Deimos">
+				<div class="orbit_deimos"></div>
 				<img src="images/deimos.png">
 			</div>
 			<div class="Phobos">
+				<div class="orbit_phobos"></div>
 				<img src="images/phobos.png">
 			</div>
 		</div>
+
 		<div class="Asteroids">
 			<img src="images/poyas-astreoidov.png">
 		</div>
+
+		<div class="orbit_jupiter"></div>
 		<div class="Jupiter">
 			<img src="images/jupiter.png">
 			<div class="Io">
+				<div class="orbit_io"></div>
 				<img src="images/io.png">
 			</div>
 			<div class="Europe">
+				<div class="orbit_europe"></div>
 				<img src="images/europe.png">
 			</div>
 			<div class="Ganimed">
+				<div class="orbit_ganimed"></div>
 				<img src="images/ganimed.png">
 			</div>
 			<div class="Callisto">
+				<div class="orbit_callisto"></div>
 				<img src="images/callisto.png">
 			</div>
 		</div>
+
+		<div class="orbit_saturn"></div>
 		<div class="Saturn">
 			<img src="images/saturn.png">
 			<div class="Mimas">
+				<div class="orbit_mimas"></div>
 				<img src="images/mimas.png">
 			</div>
 			<div class="Callipso">
+				<div class="orbit_callipso"></div>
 				<img src="images/callipso.png">
 			</div>
 			<div class="Titan">
+				<div class="orbit_titan"></div>
 				<img src="images/titan.png">
 			</div>
 		</div>
+
+		<div class="orbit_uran"></div>
 		<div class="Uran">
 			<img src="images/uran.png">
 			<div class="Miranda">
+				<div class="orbit_miranda"></div>
 				<img src="images/miranda.png">
 			</div>
 			<div class="Oberon">
+				<div class="orbit_oberon"></div>
 				<img src="images/oberon.png">
 			</div>
 		</div>
+
+		<div class="orbit_neptun"></div>
 		<div class="Neptun">
 			<img src="images/neptun.png">
 			<div class="Proteus">
+				<div class="orbit_proteus"></div>
 				<img src="images/proteus.png">
 			</div>
 			<div class="Triton">
+				<div class="orbit_triton"></div>
 				<img src="images/triton.png">
 			</div>
 		</div>
+
+		<div class="orbit_pluto"></div>
 		<div class="Pluto">
 			<img src="images/pluton.png">
 			<div class="Haron">
+				<div class="orbit_haron"></div>
 				<img src="images/haron.png">
 			</div>
 		</div>
+
 	</body>
 </html>
